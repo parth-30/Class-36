1fd5bfabbe6c26be078bb8d3da47f1feb8753c64
parth-30 <74844729+parth-30@users.noreply.github.com>
2021-03-17 21:16:33 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var database;
+var position;
+var playerCount = 0 
+var gameState = 0
+var form, player, game;
+
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+  game = new Game();
+  game.getState()
+  game.start();
+
+}
+
+function draw(){
+  background("white");
+  
+  
+    drawSprites();
+  
+}
